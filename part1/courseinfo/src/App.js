2dea9f1e8729d728a54f775036d426d95d9ee5fc
mursit orhan <mursitorhan@hotmail.com>
2022-10-12 15:39:44 +0100

    Update App.js for exercise 1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -31,25 +31,28 @@ const Total = ({ parts }) => {
 };
 
 const App = () => {
-	const course = 'Half Stack application development';
-	const parts = [
-		{
-			name: 'Fundamentals of React',
-			exercises: 10,
-		},
-		{
-			name: 'Using props to pass data',
-			exercises: 7,
-		},
-		{
-			name: 'State of a component',
-			exercises: 14,
-		},
-	];
+	const course = {
+		name: 'Half Stack application development',
+		parts: [
+			{
+				name: 'Fundamentals of React',
+				exercises: 10,
+			},
+			{
+				name: 'Using props to pass data',
+				exercises: 7,
+			},
+			{
+				name: 'State of a component',
+				exercises: 14,
+			},
+		],
+	};
 
+	const { name, parts } = course;
 	return (
 		<div>
-			<Header course={course} />
+			<Header course={name} />
 			<Content parts={parts} />
 			<Total parts={parts} />
 		</div>
