a7155a628dd436d88c20cf843bde6b56e7238563
mursit orhan <mursitorhan@hotmail.com>
2022-10-15 11:14:25 +0100

    1.7 step2 : Expand application so it could display some statistics

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -5,8 +5,25 @@ const App = () => {
 	const [good, setGood] = useState(0);
 	const [neutral, setNeutral] = useState(0);
 	const [bad, setBad] = useState(0);
+	const all = good + neutral + bad;
+	const average = (good * 1 + bad * -1) / all;
+	const positive = good / all;
 
-	return <div>code here</div>;
+	return (
+		<div>
+			<h1>give feedback</h1>
+			<button onClick={() => setGood(good + 1)}>good</button>
+			<button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+			<button onClick={() => setBad(bad + 1)}>bad</button>
+			<h1>statistics</h1>
+			<div>good {good}</div>
+			<div>neutral {neutral}</div>
+			<div>bad {bad}</div>
+			<div>all {all}</div>
+			<div>average {average}</div>
+			<div>positive {positive * 100}%</div>
+		</div>
+	);
 };
 
 export default App;
