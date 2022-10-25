95438f942709501126a8363501c4be12b447d35e
mursit orhan <mursitorhan@hotmail.com>
2022-10-25 18:02:30 +0100

    Exercise 3.12.setup mongo db and save records

---
+++ part3/mongo.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+if (process.argv.length < 5) {
+	console.log(
+		'Please provide the password as an argument: node mongo.js <password> <name> <phone number>'
+	);
+	process.exit(1);
+}
+
+const password = process.argv[2];
+const name = process.argv[3];
+const number = process.argv[4];
+
+const url = `mongodb+srv://orhan:${password}@cluster0.6ogjg.mongodb.net/?retryWrites=true&w=majority`;
+
+const personSchema = new mongoose.Schema({
+	name: String,
+	number: String,
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+mongoose
+	.connect(url)
+	.then((result) => {
+		console.log('connected');
+
+		const person = new Person({
+			name,
+			number,
+		});
+
+		return person.save();
+	})
+	.then(() => {
+		console.log(`added ${name} number ${number} to phonebook`);
+		return mongoose.connection.close();
+	})
+	.catch((err) => console.log(err));
