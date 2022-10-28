867160819a6576214c23b5fb40161392586063c1
mursit orhan <mursitorhan@hotmail.com>
2022-10-28 22:25:12 +0100

    3.15: Phonebook database, step3, 3.16: Phonebook database, step4

--- part3/index.js
+++ part3/index.js
@@ -22,7 +22,6 @@ app.get('/info', (request, response) => {
 	response.send(content);
 });
 app.get('/api/persons', (request, response) => {
-	const persons = [];
 	Person.find({}).then((persons) => {
 		response.json(persons);
 	});
@@ -45,18 +44,50 @@ app.post('/api/persons', (request, response) => {
 		response.json(savedPerson);
 	});
 });
-app.get('/api/persons/:id', (request, response) => {
-	const id = Number(request.params.id);
-	Person.findById(id).then((person) => {
-		response.json(person);
-	});
+app.get('/api/persons/:id', (request, response, next) => {
+	const { id } = request.params;
+	Person.findById(id)
+		.then((person) => {
+			if (person) {
+				response.json(person);
+			} else {
+				response.status(404).end();
+			}
+		})
+		.catch((error) => next(error));
 });
 app.delete('/api/persons/:id', (request, response) => {
-	const id = Number(request.params.id);
-	persons = persons.filter((persons) => persons.id !== id);
-
-	response.status(204).end();
+	const { id } = request.params;
+	Person.findByIdAndDelete(id)
+		.then((result) => response.status(204).end())
+		.catch((error) => next(error));
 });
+app.put('/api/persons/:id', (request, response) => {
+	const body = request.body;
+
+	const { id, number } = body;
+	const person = { number };
+
+	Person.findByIdAndUpdate(id, person)
+		.then((result) => response.status(204).end())
+		.catch((error) => next(error));
+});
+const unknownEndpoint = (request, response) => {
+	response.status(404).send({ error: 'unknown endpoint' });
+};
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint);
+const errorHandler = (error, request, response, next) => {
+	console.error(error.message);
+
+	if (error.name === 'CastError') {
+		return response.status(400).send({ error: 'malformatted id' });
+	}
+
+	next(error);
+};
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 
