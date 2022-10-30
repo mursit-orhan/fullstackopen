1fb1d56623bcdc8f2107ae89d160e0eee5069e62
mursit orhan <mursitorhan@hotmail.com>
2022-10-30 18:05:57 +0000

    3.20*: Phonebook database, step8

--- part3/models/person.js
+++ part3/models/person.js
@@ -19,7 +19,13 @@ const personSchema = new mongoose.Schema({
 	},
 	number: {
 		type: String,
-		required: true,
+		validate: {
+			validator: function (v) {
+				return /\d{2,3}-\d{7,8}/.test(v);
+			},
+			message: (props) => `${props.value} is not a valid phone number!`,
+		},
+		required: [true, 'User phone number required'],
 	},
 });
 
