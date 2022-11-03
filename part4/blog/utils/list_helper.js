a33faea5713506386e8aec036ada8d0cfac7d6cc
mursit orhan <mursitorhan@hotmail.com>
2022-11-03 20:21:15 +0000

    4.5*: helper functions and unit tests, step3

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -5,8 +5,18 @@ const totalLikes = (blogs) => {
   const likesTotal = blogs.reduce((a, b) => a + b.likes, 0)
   return likesTotal
 }
-
+const favoriteBlog = (blogs) => {
+  let mostLiked = blogs[0]
+  for (const blog of blogs) {
+    if (blog.likes > mostLiked.likes) {
+      mostLiked = blog
+    }
+  }
+  const { title, author, likes } = mostLiked
+  return { title, author, likes }
+}
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
