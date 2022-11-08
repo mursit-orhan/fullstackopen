75c2d28d7538e2d99961e25da05d01938f7e8f1a
mursit orhan <mursitorhan@hotmail.com>
2022-11-08 09:27:47 +0000

    4.15: bloglist expansion, step3

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -1,9 +1,11 @@
 const express = require('express')
-
 const app = express()
+require('express-async-errors')
 
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
---
+++ part4/blog/controllers/users.js
@@ -0,0 +1,45 @@
+const bcrypt = require('bcrypt')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+
+usersRouter.get('/', async (request, response) => {
+  const blogs = await User.find({})
+  response.json(blogs)
+})
+
+usersRouter.post('/', async (request, response) => {
+  const { username, name, password } = request.body
+
+  const existingUser = await User.findOne({ username })
+  if (existingUser) {
+    return response.status(400).json({ error: 'username must be unique' })
+  }
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(password, saltRounds)
+
+  const user = new User({
+    username,
+    name,
+    passwordHash,
+  })
+
+  const savedUser = await user.save()
+
+  response.status(201).json(savedUser)
+})
+
+usersRouter.put('/:id', async (request, response) => {
+  console.log(request.body)
+  await User.findByIdAndUpdate(request.params.id, request.body, {
+    runValidators: true,
+    context: 'query',
+  })
+  response.status(204).end()
+})
+
+usersRouter.delete('/:id', async (request, response) => {
+  await User.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
+
+module.exports = usersRouter
